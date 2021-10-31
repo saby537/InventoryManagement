63b0e806ed5a8e3a7cb5548cd88b983ddf428e24
shubhrajyotidey <toshubhrajyotidey@gmail.com>
2021-10-31 17:25:05 +0530

    Now adds to item also when adding stock

--- backend/src/routes/AddStock.ts
+++ backend/src/routes/AddStock.ts
@@ -1,10 +1,15 @@
 import express from 'express'
+import mongoose from 'mongoose';
 import AddStock from '../models/AddStock';
+import Item from '../models/Item';
 
 const router=express.Router();
 
 interface AddStockPost {
     body : {
+        Name : string,
+        Type : string,
+        SubType : string,
         ProductName : string,
         Unit : String,
         Quantity : Number,
@@ -14,7 +19,6 @@ interface AddStockPost {
         User : string
     }
 }
-
 interface EnterpriseGetByInvoice {
     body : {
         Invoice : String,
@@ -23,12 +27,57 @@ interface EnterpriseGetByInvoice {
 
 router.post("/",async (request : AddStockPost,response : any) =>  {
     try {
-        let newAddStock = new AddStock(request.body)
-        let validationFail = newAddStock.validateSync()
-        if(!validationFail){
-            newAddStock.save(); 
-            response.send("New Stock added")     
-        }else{response.send({"validate":validationFail})}   
+        const session = await mongoose.startSession();
+        session.startTransaction()
+        // let newAddStock = new AddStock(request.body)
+        // let validationFail = newAddStock.validateSync()
+        // if(!validationFail){
+        //     let val = await Item.updateOne({
+        //         Name : request.body.ProductName,
+        //         Unit : request.body.Unit,
+        //         Type : request.body.Type,
+        //         SubType : request.body.SubType,
+        //     },{
+        //         $inc : {
+        //             Quantity : request.body.Quantity
+        //         }
+        //     }).session(session);
+        //     if(! val.nModified ) {
+        //         let newItem = new Item(request.body);
+        //         let validationFail = newItem.validateSync()
+        //         if(!validationFail){
+        //             newItem.save();
+        //             newAddStock.save(); 
+        //         }else{response.send(validationFail)}   
+        //     }else{
+        //         newAddStock.save(); 
+        //     }
+        
+        await AddStock.create([{
+            Quantity : request.body.Quantity,
+            Unit : request.body.Unit,
+            ProductName : request.body.ProductName,
+            Supplier : request.body.Supplier,
+            Warehouse : request.body.Warehouse,
+            User : request.body.User,
+            Invoice : request.body.Invoice
+        }],{
+            session : session
+        })
+        await Item.updateOne({
+            Name : request.body.Name,
+            Unit : request.body.Unit,
+            Type : request.body.Type,
+            SubType : request.body.SubType,
+        },{
+            $inc : {
+                Quantity : request.body.Quantity
+            }
+        }).session(session);
+        await session.commitTransaction(); 
+        session.endSession();
+        response.send("Item and Stock updated")
+        // }else{response.send({"validate":validationFail})}   
     } catch (error) {
         response.status(500).send({"Error" : error})
     }
